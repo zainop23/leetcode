388789d6732fe37f971ed5458faa09d662564772
zainop23 <146203602+zainop23@users.noreply.github.com>
2025-06-06 05:37:54 +0530

    added comments

--- Reverse-Words-in-a-String.cpp
+++ Reverse-Words-in-a-String.cpp
@@ -1,4 +1,17 @@
-// r
+// for O(n) space complexity
+// 1. Reverse the string
+// 2. go through each word and reverse it
+// 3. store the reversed word in another string
+// 4. return the str.substr(1) meaning it will start from str[1]
+
+// for O(1) space complexity
+// 1. reverse the string
+// 2. initialize the right, left ptr to 0
+// 3. while(i<n && i==' ') : counts the number of spaces before the word
+// 4. while(i<n && s[i]!=' ') s[right++]=s[i++] : start modifying the string
+// 5. reverse the string from reverse(s.begin()+left, s.begin()+right); then left = right
+// 6. add a space in the end
+// 7. resize the string to remove the last space
 class Solution {
 public:
     string reverseWords(string s) {
@@ -17,6 +30,8 @@ public:
          }
          if(right>0) s.resize(right-1);
          return s;
+        
+        //brute force
         // int i=0;
         // string ans;
         // while(i<n){
