0ada75023b85e911878fb2ec93564ab4c88f346e
zainop23 <146203602+zainop23@users.noreply.github.com>
2025-06-06 06:59:16 +0530

    docs

--- Largest-Odd-Number-in-String.cpp
+++ Largest-Odd-Number-in-String.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=n-1; i>=0; i--){
             // if((int)(num[i])%2!=0) return num.substr(0,i+1); (beats 33%)
           //if((num[i]-'0')%2!=0)  return num.substr(0,i+1); // beats 100%
-           if(num[i] & 1)  return num.substr(0,i+1);
+           if(num[i] & 1)  return num.substr(0,i+1); //beats 100%
 
         }
 return "";
