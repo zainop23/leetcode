79834ddb42e971259805a504bc4d802362baaee7
Zain Al Abidin <146203602+zainop23@users.noreply.github.com>
2025-10-01 21:31:29 +0530

    Update Rotate-Array.cpp

--- Rotate-Array.cpp
+++ Rotate-Array.cpp
@@ -1,3 +1,4 @@
+// O(n) TC and O(1) SC
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -9,16 +10,16 @@ public:
 };
 
 // O(n) SC
-// class Solution {
-// public:
-//     void rotate(vector<int>& nums, int k) {
-//         int n= nums.size();
-//         vector<int> res(n);
-//         for(int i = 0; i<n; i++){
-//             res[(i+k)%n]=nums[i];
-//         }
-//         for(int i=0; i<n; i++){
-//             nums[i]=res[i];
-//         }
-//     }
-// };
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+        vector<int> res(n);
+        for(int i = 0; i<n; i++){
+            res[(i+k)%n]=nums[i];
+        }
+        for(int i=0; i<n; i++){
+            nums[i]=res[i];
+        }
+    }
+};
