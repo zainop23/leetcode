325f9fea2289b45f19da4da3df00d342ad37e742
Zain Al Abidin <146203602+zainop23@users.noreply.github.com>
2025-10-01 21:27:21 +0530

    [189] [Time Beats: 9.78%] [Memory Beats: 98.56%] - LeetPush

---
+++ Rotate-Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
+
+// O(n) SC
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n= nums.size();
+//         vector<int> res(n);
+//         for(int i = 0; i<n; i++){
+//             res[(i+k)%n]=nums[i];
+//         }
+//         for(int i=0; i<n; i++){
+//             nums[i]=res[i];
+//         }
+//     }
+// };
